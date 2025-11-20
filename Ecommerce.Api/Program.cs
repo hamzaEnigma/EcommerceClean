9233b82cf31af66383d096389ed82d3e33e68c38
somai hamza <HAMZASOMAI@outlook.com>
2025-11-20 11:49:43 +0100

    Set up project architecture

--- Ecommerce.Api/Program.cs
+++ Ecommerce.Api/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(); // basic; see “Extras” below for options
 
 var app = builder.Build();
 
